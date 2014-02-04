567d8081ac7c76478f7ed7e98dad6a89376beaf9
mheine <mheine@kth.se>
2014-02-04 11:38:59 +0100

    Updated some stuffs yo

--- src/heine/Window_1.java
+++ src/heine/Window_1.java
@@ -2,16 +2,26 @@ package heine;
 
 import javax.swing.*;
 
+@SuppressWarnings("serial")
 public class Window_1 extends JFrame {
 	
 	public Window_1() {
-
+		
 		setSize(500, 500);
 		setVisible(true);
 		
-	}
-	
-	
+		setLocationRelativeTo(null);
+		setTitle("Ett fönster! Sjukt kul.");
+		
+		
+		
+		JPanel mainPanel = new JPanel();
+		this.add(mainPanel);
+		
+		JButton clickButton = new JButton("Klicka här!");
+		mainPanel.add(clickButton);
+		
+	}	
 	
 	public static void main(String [] args) {
 		
--- src/heine/Window_2.java
+++ src/heine/Window_2.java
@@ -1,8 +1,14 @@
 package heine;
 
+import java.awt.*;
+import java.awt.event.*;
+
 import javax.swing.*;
 
-public class Window_2 extends JFrame {
+public class Window_2 extends JFrame implements ActionListener {
+	
+	private JButton titleButton;
+	private JTextArea textarea;
 	
 	public Window_2() {
 		
@@ -11,14 +17,22 @@ public class Window_2 extends JFrame {
 		JPanel mainPanel = new JPanel();
 		this.add(mainPanel);
 		
-		JButton clickButton = new JButton("Klicka här!");
-		mainPanel.add(clickButton);
+		titleButton = new JButton("Byt titel");
+		titleButton.addActionListener(this);
+
+		textarea = new JTextArea("", 2, 20);
+		
+
+		mainPanel.add(titleButton);
+		mainPanel.add(textarea);
 		
 	}
 	
 	private void init() {
-		setSize(500, 500);
+		setSize(500, 100);
 		setVisible(true);
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		setLocationRelativeTo(null);
 		setTitle("Ett fönster! Sjukt kul.");
@@ -29,6 +43,16 @@ public class Window_2 extends JFrame {
 	public static void main(String [] args) {
 		
 		new Window_2();   
+		
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource() == titleButton) {
+			String title = textarea.getText();
+			this.setTitle(title);
+
+		}
 		
 	}
 
